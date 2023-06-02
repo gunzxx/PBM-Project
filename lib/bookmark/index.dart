a701902b2678e8d22c50b6f575c32f3b61774aca
gunzxx <ggm5850@gmail.com>
2023-06-02 23:18:25 +0700

    fix bookmark

--- lib/bookmark/index.dart
+++ lib/bookmark/index.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 import '../mylib/bookmark.dart';
 import '../mylib/color.dart';
@@ -17,6 +16,7 @@ class BookMark extends StatefulWidget {
 
 class _BookMarkState extends State<BookMark> {
   late Future _bookmarks;
+  bool _onLoading = false;
 
   @override
   initState() {
@@ -46,90 +46,106 @@ class _BookMarkState extends State<BookMark> {
         child: FutureBuilder(
           future: _bookmarks,
           builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
+            if (snapshot.hasError) {
               return const Center(
-                child: SpinKitCubeGrid(
-                  color: bl1,
-                  size: 50.0,
-                ),
+                child: Text("Terjadi kesalahan."),
               );
             } else if (snapshot.hasData) {
               final tourists = jsonDecode(snapshot.data!);
+              _onLoading = false;
               return GridView.builder(
                 itemCount: tourists.length,
+                padding: EdgeInsets.all(10),
                 gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                   crossAxisCount: 2,
-                  childAspectRatio: 1.0,
+                  childAspectRatio: 1 / 1.2,
                   mainAxisSpacing: 10,
                   crossAxisSpacing: 10,
                 ),
                 itemBuilder: (context, index) {
-                  return Card(
-                    margin: EdgeInsets.all(10),
-                    child: GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) =>
-                                    DetailTouristPage(tourists[index])));
-                      },
-                      child: Container(
-                        margin: const EdgeInsets.symmetric(horizontal: 10),
-                        padding: const EdgeInsets.all(10),
-                        width: 150,
-                        decoration: BoxDecoration(
-                            color: w1,
-                            borderRadius: BorderRadius.circular(10),
-                            boxShadow: const [
-                              BoxShadow(
-                                color: Color.fromRGBO(0, 0, 0, .1),
-                                blurRadius: 10,
-                              )
-                            ]),
-                        child: Column(
-                          children: [
-                            AspectRatio(
-                              aspectRatio: 16 / 9,
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(5),
-                                child: Image.network(
-                                  tourists[index]['thumb'],
-                                  fit: tourists[index]['thumb'] !=
-                                          "https://paa.gunzxx.my.id/img/tourist/default.png"
-                                      ? BoxFit.cover
-                                      : BoxFit.contain,
-                                  loadingBuilder:
-                                      (context, child, loadingProgress) {
-                                    if (loadingProgress == null) {
-                                      return child;
-                                    }
-                                    return Center(
-                                        child: CircularProgressIndicator(
-                                      value:
-                                          loadingProgress.expectedTotalBytes !=
-                                                  null
-                                              ? loadingProgress
-                                                      .cumulativeBytesLoaded /
-                                                  loadingProgress
-                                                      .expectedTotalBytes!
-                                              : null,
-                                    ));
-                                  },
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  DetailTouristPage(tourists[index])));
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.all(10),
+                      width: 150,
+                      decoration: BoxDecoration(
+                          color: w1,
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: const [
+                            BoxShadow(
+                              color: Color.fromRGBO(0, 0, 0, .1),
+                              blurRadius: 10,
+                            )
+                          ]),
+                      child: Stack(
+                        children: [
+                          Column(
+                            children: [
+                              AspectRatio(
+                                aspectRatio: 16 / 9,
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(5),
+                                  child: Image.network(
+                                    tourists[index]['thumb'],
+                                    fit: tourists[index]['thumb'] !=
+                                            "https://paa.gunzxx.my.id/img/tourist/default.png"
+                                        ? BoxFit.cover
+                                        : BoxFit.contain,
+                                    loadingBuilder:
+                                        (context, child, loadingProgress) {
+                                      if (loadingProgress == null) {
+                                        return child;
+                                      }
+                                      return Center(
+                                          child: CircularProgressIndicator(
+                                        value: loadingProgress
+                                                    .expectedTotalBytes !=
+                                                null
+                                            ? loadingProgress
+                                                    .cumulativeBytesLoaded /
+                                                loadingProgress
+                                                    .expectedTotalBytes!
+                                            : null,
+                                      ));
+                                    },
+                                  ),
                                 ),
                               ),
-                            ),
-                            const SizedBox(height: 5),
-                            Text(
-                              tourists[index]['name'],
-                              textAlign: TextAlign.center,
-                            ),
-                            const SizedBox(height: 5),
-                            Text(strLimit(tourists[index]['description'], 15),
+                              const SizedBox(height: 5),
+                              Text(
+                                tourists[index]['name'],
                                 textAlign: TextAlign.center,
-                                style: const TextStyle(fontSize: 12)),
-                          ],
-                        ),
+                              ),
+                              const SizedBox(height: 5),
+                              Text(strLimit(tourists[index]['description'], 15),
+                                  textAlign: TextAlign.center,
+                                  style: const TextStyle(fontSize: 12)),
+                            ],
+                          ),
+                          Positioned(
+                            bottom: 0,
+                            right: 0,
+                            child: IconButton(
+                                onPressed: _onLoading == true
+                                    ? null
+                                    : () async {
+                                        await removeBookmark(
+                                            tourists[index]['id']);
+                                        setState(() {
+                                          _bookmarks = getBookmark();
+                                          _onLoading = true;
+                                        });
+                                      },
+                                padding: EdgeInsets.zero,
+                                icon: Icon(Icons.delete)),
+                          ),
+                        ],
                       ),
                     ),
                   );
